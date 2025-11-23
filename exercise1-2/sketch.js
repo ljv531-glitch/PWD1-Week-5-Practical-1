97bb426393d7de949b22e5ad506b9538ec6625da
ljv531-glitch <ljv531@york.ac.uk>
2025-11-23 16:49:55 +0000

    Update sketch.js

--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -1,22 +1,48 @@
-function setup() {
+let starXs = [];
+let starYs = [];
 
+function setup() {
+  createCanvas(500, 500); 
 }
 
 function draw() {
+  background(20, 30, 60); 
 
+  updateStars();
+  drawStars();
+}
+
+
+function updateStars() {
+  for (let i = 0; i < starYs.length; i++) {
+    starYs[i] += 2; 
+  }
+}
+
+
+function drawStars() {
+  for (let i = 0; i < starXs.length; i++) {
+    star(starXs[i], starYs[i]);
+  }
+}
+
+function mousePressed() {
+  
+  starXs.push(mouseX);
+  starYs.push(mouseY);
 }
 
 /**
- * Draws a star at the given coordinates.
+ * 
  * @param {number} x 
  * @param {number} y 
  */
 function star(x, y) {
-    fill(255, 234, 0);
-    noStroke();
-    triangle(x, y - 50, x - 20, y, x + 20, y);
-    triangle(x - 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x + 50, y - 20, x, y - 20, x, y + 10);
-    triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
-    triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
+  fill(255, 234, 0);
+  noStroke();
+  triangle(x, y - 50, x - 20, y, x + 20, y);
+  triangle(x - 50, y - 20, x, y - 20, x, y + 10);
+  triangle(x + 50, y - 20, x, y - 20, x, y + 10);
+  triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
+  triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
 }
